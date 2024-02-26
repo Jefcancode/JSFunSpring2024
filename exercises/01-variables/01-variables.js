f8c96dfd22a67e9ba2f37705e585fb19e8761581
Jefcancode <jeffcancode@gmail.com>
2024-02-26 17:38:17 -0500

    Did second  exer

--- exercises/01-variables/01-variables.js
+++ exercises/01-variables/01-variables.js
@@ -5,3 +5,23 @@
 
 // WRITE YOUR ANSWER BELOW THIS LINE
 let myNumber =0;
+
+
+
+const myString ="Sooo ";
+const myStringShortened=myString.substring(0,2);
+console.log(myStringShortened);
+
+
+const firstTwoCharacters=myString.substring(0,2);
+
+
+const firstTwoLetters=name.substring(0,3);
+
+
+const fullname ="Coco Chanel";
+console.log(fullname.indexOf(' '));
+let spaceIndex=fullname.indexOf('')+1;
+console.log(spaceIndex);
+console.log(fullname.substring(spaceIndex));
+//console.log(lastName);
--- exercises/01-variables/02-variables.js
+++ exercises/01-variables/02-variables.js
@@ -4,3 +4,4 @@
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+var year=2024;
